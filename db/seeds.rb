86f180a231c393da49a9c2f158b9e684ccec96a3
Felix Nagel <felixna7@gmail.com>
2020-05-21 16:45:09 +0200

    mvc almost finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+drinks = JSON.parse(open(url).read)["drinks"]
+
+drinks.each do |drink|
+  Ingredient.create!(name: drink["strIngredient1"])
+end
+
+mojito = Cocktail.create!(name: 'Mojito')
+bloody = Cocktail.create!(name: 'Bloody Mary')
+sex = Cocktail.create!(name: 'Sex on the Beach')
+
+cocktails = [mojito, bloody, sex]
+cocktails.each do |cocktail|
+  puts "#{cocktail.name} created!"
+end
