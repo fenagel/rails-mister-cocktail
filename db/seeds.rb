46caf149ba6e3e4978730c357c663cf862e34da4
Felix Nagel <felixna7@gmail.com>
2020-05-22 16:44:12 +0200

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,16 @@ drinks.each do |drink|
   Ingredient.create!(name: drink["strIngredient1"])
 end
 
+file_mojito = URI.open('https://source.unsplash.com/1600x900/?mojito')
+file_bloody = URI.open('https://source.unsplash.com/1600x900/?bloody-mary')
+file_sex = URI.open('https://source.unsplash.com/1600x900/?cocktail')
+
 mojito = Cocktail.create!(name: 'Mojito')
+mojito.photo.attach(io: file_mojito, filename: 'mojito.jpg', content_type: 'image/jpg')
 bloody = Cocktail.create!(name: 'Bloody Mary')
+bloody.photo.attach(io: file_bloody, filename: 'bloody.jpg', content_type: 'image/jpg')
 sex = Cocktail.create!(name: 'Sex on the Beach')
+sex.photo.attach(io: file_sex, filename: 'sex_on_the_beach.jpg', content_type: 'image/jpg')
 
 cocktails = [mojito, bloody, sex]
 cocktails.each do |cocktail|
