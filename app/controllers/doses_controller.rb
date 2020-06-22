8c335b200ee455123ae2e4242f65769ee1a6db1b
Felix Nagel <felixna7@gmail.com>
2020-06-22 14:32:02 +0200

    updates

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,12 +1,10 @@
 class DosesController < ApplicationController
-
+  before_action :set_cocktail, only: [:new, :create, :edit, :update]
   def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
@@ -22,7 +20,21 @@ class DosesController < ApplicationController
     redirect_to cocktail_path(@dose.cocktail)
   end
 
+  def edit
+    @dose = Dose.find(params[:id])
+  end
+
+  def update
+    @dose = Dose.find(params[:id])
+    @dose.update(dose_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
   private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'open-uri'
 
+Dose.destroy_all
 Ingredient.destroy_all
 Cocktail.destroy_all
 
@@ -12,18 +13,55 @@ drinks.each do |drink|
   Ingredient.create!(name: drink["strIngredient1"])
 end
 
-file_mojito = URI.open('https://source.unsplash.com/1600x900/?mojito')
-file_bloody = URI.open('https://source.unsplash.com/1600x900/?bloody-mary')
-file_sex = URI.open('https://source.unsplash.com/1600x900/?cocktail')
+# file_mojito = URI.open('https://source.unsplash.com/1600x900/?mojito')
+# file_bloody = URI.open('https://source.unsplash.com/1600x900/?bloody-mary')
+# file_sex = URI.open('https://source.unsplash.com/1600x900/?cocktail')
 
-mojito = Cocktail.create!(name: 'Mojito')
-mojito.photo.attach(io: file_mojito, filename: 'mojito.jpg', content_type: 'image/jpg')
-bloody = Cocktail.create!(name: 'Bloody Mary')
-bloody.photo.attach(io: file_bloody, filename: 'bloody.jpg', content_type: 'image/jpg')
-sex = Cocktail.create!(name: 'Sex on the Beach')
-sex.photo.attach(io: file_sex, filename: 'sex_on_the_beach.jpg', content_type: 'image/jpg')
+# mojito = Cocktail.create!(name: 'Mojito')
+# mojito.photo.attach(io: file_mojito, filename: 'mojito.jpg', content_type: 'image/jpg')
+# bloody = Cocktail.create!(name: 'Bloody Mary')
+# bloody.photo.attach(io: file_bloody, filename: 'bloody.jpg', content_type: 'image/jpg')
+# long = Cocktail.create!(name: 'Long Island Iced Tea')
+# long.photo.attach(io: file_long, filename: 'long.jpg', content_type: 'image/jpg')
 
-cocktails = [mojito, bloody, sex]
+# cocktails = [mojito, bloody, long]
+# cocktails.each do |cocktail|
+#   puts "#{cocktail.name} created!"
+# end
+
+mojito = Cocktail.create!(name: "Mojito")
+mojito_image = URI.open("http://res.cloudinary.com/fenagel/image/upload/v1592815879/t7uu4r7xondd1tzubgjb.jpg")
+mojito.photo.attach(io: mojito_image, filename: 'mojito.jpg', content_type: 'image/jpg')
+
+bloody = Cocktail.create!(name: "Bloody Mary")
+bloody_image = URI.open("http://res.cloudinary.com/fenagel/image/upload/v1592815911/edulfe0rcpdvgenebptr.jpg")
+bloody.photo.attach(io: bloody_image, filename: 'bloody.jpg', content_type: 'image/jpg')
+
+long = Cocktail.create!(name: "Long Island Iced Tea")
+long_image = URI.open("http://res.cloudinary.com/fenagel/image/upload/v1592815941/j4xybawdtovr56xvboam.jpg")
+long.photo.attach(io: long_image, filename: 'long.jpg', content_type: 'image/jpg')
+
+aperol = Cocktail.create!(name: "Aperol Spritz")
+aperol_image = URI.open("http://res.cloudinary.com/fenagel/image/upload/v1592815972/od0nprqhbpfkmpz1prjj.jpg")
+aperol.photo.attach(io: aperol_image, filename: 'aperol.jpg', content_type: 'image/jpg')
+
+blue = Cocktail.create!(name: "Blue Lagoon")
+blue_image = URI.open("https://res.cloudinary.com/fenagel/image/upload/v1592816121/attyts38c9xqacfzudcf.jpg")
+blue.photo.attach(io: blue_image, filename: 'blue.jpg', content_type: 'image/jpg')
+
+old = Cocktail.create!(name: "Old Fashioned")
+old_image = URI.open("http://res.cloudinary.com/fenagel/image/upload/v1592816248/lgwh7mh3ugrdkrqg7oaj.jpg")
+old.photo.attach(io: old_image, filename: 'old.jpg', content_type: 'image/jpg')
+
+yellow = Cocktail.create!(name: "Yellow Bird")
+yellow_image = URI.open("https://res.cloudinary.com/fenagel/image/upload/v1592817009/i9j4anopcgfj3yffbhur.jpg")
+yellow.photo.attach(io: yellow_image, filename: 'yellow.jpg', content_type: 'image/jpg')
+
+black = Cocktail.create!(name: "Black Rumhattan")
+black_image = URI.open("http://res.cloudinary.com/fenagel/image/upload/v1592817224/wqrltvtrnctjim9kzvqi.jpg")
+black.photo.attach(io: black_image, filename: 'black.jpg', content_type: 'image/jpg')
+
+cocktails = [mojito, bloody, long, aperol, old, yellow, black]
 cocktails.each do |cocktail|
   puts "#{cocktail.name} created!"
 end
